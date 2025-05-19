9e925b8cb03352808a70a5c26e83c1880cc0d599
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2025-05-19 14:17:20 -0400

    Create get-dependencies.sh

---
+++ get-dependencies.sh
@@ -0,0 +1,46 @@
+#!/bin/sh
+
+set -eux
+
+sed -i 's/DownloadUser/#DownloadUser/g' /etc/pacman.conf
+
+if [ "$(uname -m)" = 'x86_64' ]; then
+	PKG_TYPE='x86_64.pkg.tar.zst'
+else
+	PKG_TYPE='aarch64.pkg.tar.xz'
+fi
+
+MESA_URL="https://github.com/pkgforge-dev/llvm-libs-debloated/releases/download/continuous/mesa-mini-$PKG_TYPE"
+LIBXML_URL="https://github.com/pkgforge-dev/llvm-libs-debloated/releases/download/continuous/libxml2-iculess-$PKG_TYPE"
+OPUS_URL="https://github.com/pkgforge-dev/llvm-libs-debloated/releases/download/continuous/opus-nano-$PKG_TYPE"
+
+echo "Installing build dependencies..."
+echo "---------------------------------------------------------------"
+pacman -Syu --noconfirm \
+  alacritty \
+	base-devel \
+	curl \
+	desktop-file-utils \
+	git \
+	libxtst \
+	patchelf \
+	pipewire-audio \
+	pulseaudio \
+	pulseaudio-alsa \
+	strace \
+	wget \
+	xorg-server-xvfb \
+	zsync
+
+echo "Installing debloated pckages..."
+echo "---------------------------------------------------------------"
+wget --retry-connrefused --tries=30 "$MESA_URL"   -O  ./mesa.pkg.tar.zst
+wget --retry-connrefused --tries=30 "$LIBXML_URL" -O  ./libxml2.pkg.tar.zst
+wget --retry-connrefused --tries=30 "$OPUS_URL"   -O  ./opus.pkg.tar.zst
+
+pacman -U --noconfirm ./*.pkg.tar.zst
+rm -f ./*.pkg.tar.zst
+
+
+echo "All done!"
+echo "---------------------------------------------------------------"

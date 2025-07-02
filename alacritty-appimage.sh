3a71ae89feb9fbce7eda2483a655d684a317cc9b
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2025-07-02 22:41:53 +0000

    build alacritty manually (#4)
    Nothing to see here!

--- alacritty-appimage.sh
+++ alacritty-appimage.sh
@@ -1,73 +1,77 @@
 #!/bin/sh
 
-set -eux
+set -ex
 
-PACKAGE=alacritty
-DESKTOP=Alacritty.desktop
-ICON=Alacritty.svg
-
-export ARCH="$(uname -m)"
-export APPIMAGE_EXTRACT_AND_RUN=1
-export VERSION=$(pacman -Q "$PACKAGE" | awk 'NR==1 {print $2; exit}')
-echo "$VERSION" > ~/version
-
-UPINFO="gh-releases-zsync|$(echo "$GITHUB_REPOSITORY" | tr '/' '|')|latest|*$ARCH.AppImage.zsync"
-LIB4BN="https://raw.githubusercontent.com/VHSgunzo/sharun/refs/heads/main/lib4bin"
+ARCH="$(uname -m)"
+VERSION="$(cat ~/version)"
 URUNTIME="https://github.com/VHSgunzo/uruntime/releases/latest/download/uruntime-appimage-dwarfs-$ARCH"
+URUNTIME_LITE="https://github.com/VHSgunzo/uruntime/releases/latest/download/uruntime-appimage-dwarfs-lite-$ARCH"
+SHARUN="https://github.com/VHSgunzo/sharun/releases/latest/download/sharun-$ARCH-aio"
+UPINFO="gh-releases-zsync|$(echo "$GITHUB_REPOSITORY" | tr '/' '|')|latest|*$ARCH.AppImage.zsync"
+
 # Prepare AppDir
-mkdir -p ./AppDir
-cd ./AppDir
+mkdir -p ./AppDir/shared/bin
+cp -v ./alacritty/target/release/alacritty             ./AppDir/shared/bin 
+cp -v ./alacritty/extra/linux/Alacritty.desktop        ./AppDir
+cp -v ./alacritty/extra/logo/compat/alacritty-term.svg ./AppDir
+cp -v ./alacritty/extra/logo/compat/alacritty-term.svg ./AppDir/.DirIcon
 
-cp -v /usr/share/applications/"$DESKTOP" ./
-cp -v /usr/share/pixmaps/"$ICON"         ./
-cp -v /usr/share/pixmaps/"$ICON"         ./.DirIcon
-
-# ADD LIBRARIES
-wget "$LIB4BN" -O ./lib4bin
-chmod +x ./lib4bin
-xvfb-run -d -- ./lib4bin -p -v -e -s -k \
-	/usr/bin/alacritty \
-	/usr/lib/lib*GL* \
-	/usr/lib/dri/* \
-	/usr/lib/libXss.so* \
-	/usr/lib/pulseaudio/*
-
-# Prepare sharun
-echo 'unset ARGV0' > ./.env
-ln ./sharun ./AppRun
-./sharun -g
+rm -rf ./alacritty && ( 
+	cd ./AppDir
+	# ADD LIBRARIES
+	wget --retry-connrefused --tries=30 "$SHARUN" -O ./sharun-aio
+	chmod +x ./sharun-aio
+	xvfb-run -a -- \
+		./sharun-aio l -p -v -e -s -k \
+		./shared/bin/alacritty        \
+		/usr/lib/lib*GL*              \
+		/usr/lib/dri/*                \
+		/usr/lib/gbm/*                \
+		/usr/lib/libXss.so*           \
+		/usr/lib/pulseaudio/*
+	rm -f ./sharun-aio
+	
+	# Prepare sharun
+	echo 'unset ARGV0' > ./.env
+	ln ./sharun ./AppRun
+	./sharun -g
+)
 
 # MAKE APPIMAGE WITH URUNTIME
-cd ..
-wget -q "$URUNTIME" -O ./uruntime
-chmod +x ./uruntime
+wget --retry-connrefused --tries=30 "$URUNTIME"      -O  ./uruntime
+wget --retry-connrefused --tries=30 "$URUNTIME_LITE" -O  ./uruntime-lite
+chmod +x ./uruntime*
 
 # Keep the mount point (speeds up launch time)
-sed -i 's|URUNTIME_MOUNT=[0-9]|URUNTIME_MOUNT=0|' ./uruntime
+sed -i 's|URUNTIME_MOUNT=[0-9]|URUNTIME_MOUNT=0|' ./uruntime-lite
 
-#Add udpate info to runtime
+# Add udpate info to runtime
 echo "Adding update information \"$UPINFO\" to runtime..."
-./uruntime --appimage-addupdinfo "$UPINFO"
+./uruntime-lite --appimage-addupdinfo "$UPINFO"
 
 echo "Generating AppImage..."
-./uruntime --appimage-mkdwarfs -f \
-	--set-owner 0 --set-group 0 \
-	--no-history --no-create-timestamp \
+./uruntime \
+	--appimage-mkdwarfs -f               \
+	--set-owner 0 --set-group 0          \
+	--no-history --no-create-timestamp   \
 	--compression zstd:level=22 -S26 -B8 \
-	--header uruntime \
-	-i ./AppDir -o "$PACKAGE"-"$VERSION"-anylinux-"$ARCH".AppImage
+	--header uruntime-lite               \
+	-i ./AppDir                          \
+	-o ./alacritty-"$VERSION"-anylinux-"$ARCH".AppImage
 
 # make appbundle
-UPINFO="gh-releases-zsync|$(echo "$GITHUB_REPOSITORY" | tr '/' '|')|latest|*$ARCH*.AppBundle.zsync"
-wget -qO ./pelf "https://github.com/xplshn/pelf/releases/latest/download/pelf_$ARCH"
+UPINFO="$(echo "$UPINFO" | sed 's#.AppImage.zsync#*.AppBundle.zsync#g')"
+wget --retry-connrefused --tries=30 \
+	"https://github.com/xplshn/pelf/releases/latest/download/pelf_$ARCH" -O ./pelf
 chmod +x ./pelf
-echo "Generating [dwfs]AppBundle...(Go runtime)"
-./pelf --add-appdir ./AppDir \
-	--compression "-C zstd:level=22 -S26 -B8" \
-	--appbundle-id="$PACKAGE-$VERSION" \
+echo "Generating [dwfs]AppBundle..."
+./pelf \
+	--compression "-C zstd:level=22 -S26 -B8"      \
+	--appbundle-id="alacritty-$VERSION"            \
 	--appimage-compat --disable-use-random-workdir \
-	--add-updinfo "$UPINFO" \
-	--output-to "$PACKAGE-$VERSION-anylinux-$ARCH.dwfs.AppBundle"
+	--add-updinfo "$UPINFO"                        \
+	--add-appdir ./AppDir                          \
+	--output-to ./alacritty-"$VERSION"-anylinux-"$ARCH".dwfs.AppBundle
 
 zsyncmake ./*.AppImage -u ./*.AppImage
 zsyncmake ./*.AppBundle -u ./*.AppBundle
--- get-dependencies.sh
+++ get-dependencies.sh
@@ -2,13 +2,15 @@
 
 set -eux
 
-sed -i 's/DownloadUser/#DownloadUser/g' /etc/pacman.conf
+ARCH="$(uname -m)"
+REPO="https://github.com/alacritty/alacritty.git"
+GRON="https://raw.githubusercontent.com/xonixx/gron.awk/refs/heads/main/gron.awk"
 
-if [ "$(uname -m)" = 'x86_64' ]; then
-	PKG_TYPE='x86_64.pkg.tar.zst'
-else
-	PKG_TYPE='aarch64.pkg.tar.xz'
-fi
+case "$ARCH" in
+	'x86_64')  PKG_TYPE='x86_64.pkg.tar.zst';;
+	'aarch64') PKG_TYPE='aarch64.pkg.tar.xz';;
+	''|*) echo "Unknown arch: $ARCH"; exit 1;;
+esac
 
 LLVM_URL="https://github.com/pkgforge-dev/llvm-libs-debloated/releases/download/continuous/llvm-libs-nano-$PKG_TYPE"
 MESA_URL="https://github.com/pkgforge-dev/llvm-libs-debloated/releases/download/continuous/mesa-mini-$PKG_TYPE"
@@ -18,34 +20,64 @@ OPUS_URL="https://github.com/pkgforge-dev/llvm-libs-debloated/releases/download/
 echo "Installing build dependencies..."
 echo "---------------------------------------------------------------"
 pacman -Syu --noconfirm \
-	alacritty \
-	base-devel \
-	cargo \
-	cmake \
-	curl \
-	desktop-file-utils \
-	fontconfig \
-	freetype2 \
-	gdb \
-	git \
-	libxcb \
-	libxcursor \
-	libxi \
-	libxkbcommon \
-	libxkbcommon-x11 \
-	libxrandr \
-	libxtst \
-	ncurses \
-	patchelf \
-	pipewire-audio \
-	pulseaudio \
-	pulseaudio-alsa \
-	rust \
-	scdoc \
-	strace \
-	wget \
-	xorg-server-xvfb \
+	base-devel          \
+	cargo               \
+	cmake               \
+	curl                \
+	desktop-file-utils  \
+	fontconfig          \
+	freetype2           \
+	gdb                 \
+	git                 \
+	libxcb              \
+	libxcursor          \
+	libxi               \
+	libxkbcommon        \
+	libxkbcommon-x11    \
+	libxrandr           \
+	libxtst             \
+	mesa                \
+	ncurses             \
+	patch               \
+	patchelf            \
+	pipewire-audio      \
+	pulseaudio          \
+	pulseaudio-alsa     \
+	rust                \
+	scdoc               \
+	strace              \
+	wget                \
+	xorg-server-xvfb    \
 	zsync
+
+# Determine to build nightly or stable
+if [ "DEVEL" = 'true' ]; then
+	echo "Making nightly build of alacritty..."
+	VERSION="$(git ls-remote "$REPO" HEAD | cut -c 1-9)"
+	git clone --recursive -j$(nproc) "$REPO" ./alacritty
+else
+	echo "Making stable build of alacritty..."
+	wget "$GRON" -O ./gron.awk
+	chmod +x ./gron.awk
+	VERSION=$(wget https://api.github.com/repos/alacritty/alacritty/tags -O - \
+		| ./gron.awk | awk -F'=|"' '/name/ {print $3; exit}')
+	git clone --recursive -j$(nproc) --branch "$VERSION" --single-branch "$REPO" ./alacritty
+fi
+
+# We need to build alacritty here and not later as it turns out rust sucks and will fail with this error when using the smaller llvm
+# rustc: symbol lookup error: /usr/lib/librustc_driver-fa1421cc2e9f32b2.so: undefined symbol: LLVMInitializeARMTargetInfo, version LLVM_20.1
+echo "Building alacritty..."
+echo "---------------------------------------------------------------"
+
+cp -v ./hack.patch ./alacritty && (
+	cd ./alacritty
+	patch -p1 -i ./hack.patch
+	cargo fetch --locked --target "$(rustc -vV | sed -n 's/host: //p')"
+	CARGO_INCREMENTAL=0 cargo build --release --locked --offline
+	CARGO_INCREMENTAL=0 cargo test --locked --offline
+	echo "$VERSION" > ~/version
+)
+
 
 echo "Installing debloated pckages..."
 echo "---------------------------------------------------------------"

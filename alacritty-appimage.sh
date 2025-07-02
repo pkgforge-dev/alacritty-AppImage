32cd1977815b6550e6656cdae732f2ced88a7744
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2025-07-02 17:52:26 -0400

    Update alacritty-appimage.sh

--- alacritty-appimage.sh
+++ alacritty-appimage.sh
@@ -3,44 +3,17 @@
 set -ex
 
 ARCH="$(uname -m)"
-REPO="https://github.com/alacritty/alacritty.git"
-GRON="https://raw.githubusercontent.com/xonixx/gron.awk/refs/heads/main/gron.awk"
 URUNTIME="https://github.com/VHSgunzo/uruntime/releases/latest/download/uruntime-appimage-dwarfs-$ARCH"
+URUNTIME_LITE="https://github.com/VHSgunzo/uruntime/releases/latest/download/uruntime-appimage-dwarfs-lite-$ARCH"
 SHARUN="https://github.com/VHSgunzo/sharun/releases/latest/download/sharun-$ARCH-aio"
 UPINFO="gh-releases-zsync|$(echo "$GITHUB_REPOSITORY" | tr '/' '|')|latest|*$ARCH.AppImage.zsync"
 
-# Determine to build nightly or stable
-if [ "DEVEL" = 'true' ]; then
-	echo "Making nightly build of alacritty..."
-	VERSION="$(git ls-remote "$REPO" HEAD | cut -c 1-9)"
-	git clone --recursive -j$(nproc) "$REPO" ./alacritty
-else
-	echo "Making stable build of alacritty..."
-	wget "$GRON" -O ./gron.awk
-	chmod +x ./gron.awk
-	VERSION=$(wget https://api.github.com/repos/alacritty/alacritty/tags -O - \
-		| ./gron.awk | awk -F'=|"' '/name/ {print $3; exit}')
-	git clone --recursive -j$(nproc) --branch "$VERSION" --single-branch "$REPO" ./alacritty
-fi
-echo "$VERSION" > ~/version
-
-# build alacritty
-(
-	cd ./alacritty
-	#patch -p1 -i ./hack.patch
-	cargo fetch --locked --target "$(rustc -vV | sed -n 's/host: //p')"
-	CARGO_INCREMENTAL=0 cargo build --release --locked --offline
-	#CARGO_INCREMENTAL=0 cargo test --locked --offline
-
-)
-
-
 # Prepare AppDir
 mkdir -p ./AppDir/shared/bin
+cp -v ./alacritty/target/release/alacritty             ./AppDir/shared/bin 
 cp -v ./alacritty/extra/linux/Alacritty.desktop        ./AppDir
 cp -v ./alacritty/extra/logo/compat/alacritty-term.svg ./AppDir
 cp -v ./alacritty/extra/logo/compat/alacritty-term.svg ./AppDir/.DirIcon
-cp -v ./alacritty/target/release/alacritty ./AppDir/shared/bin 
 
 rm -rf ./alacritty && ( 
 	cd ./AppDir
@@ -67,12 +40,12 @@ wget --retry-connrefused --tries=30 "$URUNTIME"      -O  ./uruntime
 wget --retry-connrefused --tries=30 "$URUNTIME_LITE" -O  ./uruntime-lite
 chmod +x ./uruntime*
 
+# Keep the mount point (speeds up launch time)
+sed -i 's|URUNTIME_MOUNT=[0-9]|URUNTIME_MOUNT=0|' ./uruntime-lite
+
 # Add udpate info to runtime
 echo "Adding update information \"$UPINFO\" to runtime..."
 ./uruntime-lite --appimage-addupdinfo "$UPINFO"
-
-# Keep the mount point (speeds up launch time)
-sed -i 's|URUNTIME_MOUNT=[0-9]|URUNTIME_MOUNT=0|' ./uruntime
 
 echo "Generating AppImage..."
 ./uruntime \

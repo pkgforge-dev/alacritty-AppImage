2d37fd0b60848beb21f5d61c9294c87fb9a6bed1
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2025-08-11 01:44:26 -0400

    scripts handle `.DirIcon` and hardlink now

--- alacritty-appimage.sh
+++ alacritty-appimage.sh
@@ -11,21 +11,20 @@ UPDATER="https://raw.githubusercontent.com/pkgforge-dev/Anylinux-AppImages/refs/
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
 export OUTNAME=alacritty-"$VERSION"-anylinux-"$ARCH".AppImage
 export URUNTIME_PRELOAD=1 # really needed here
+export DEPLOY_OPENGL=1
 
 # Prepare AppDir
 mkdir -p ./AppDir/shared/bin
 cp -v ./alacritty/target/release/alacritty             ./AppDir/shared/bin 
 cp -v ./alacritty/extra/linux/Alacritty.desktop        ./AppDir
 cp -v ./alacritty/extra/logo/compat/alacritty-term.svg ./AppDir
-cp -v ./alacritty/extra/logo/compat/alacritty-term.svg ./AppDir/.DirIcon
 rm -rf ./alacritty 
 
 # ADD LIBRARIES
 wget --retry-connrefused --tries=30 "$SHARUN" -O ./quick-sharun
 chmod +x ./quick-sharun
-DEPLOY_OPENGL=1 ./quick-sharun ./AppDir/shared/bin/alacritty
+./quick-sharun ./AppDir/shared/bin/alacritty
 echo 'unset ARGV0' > ./AppDir/.env
-ln ./AppDir/sharun ./AppDir/AppRun
 
 # add self updater script, run alacritty-update in alacritty to update
 wget --retry-connrefused --tries=30 "$UPDATER" -O ./AppDir/bin/alacritty-update

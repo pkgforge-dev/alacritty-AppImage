9aa153741911b5ce948136f696a54123b41cac6f
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2025-08-23 04:45:00 -0400

    get nano packages instead

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -67,4 +67,4 @@ echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
 wget --retry-connrefused --tries=30 "$EXTRA_PACKAGES" -O ./get-debloated-pkgs.sh
 chmod +x ./get-debloated-pkgs.sh
-./get-debloated-pkgs.sh --add-opengl
+./get-debloated-pkgs.sh --add-opengl --prefer-nano
